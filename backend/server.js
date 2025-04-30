a4278fe45985be1ff0f6fb693aabbed7afd341c2
Malaika Naseer <i221508@nu.edu.com>
2025-04-30 11:56:29 +0500

    Changes made in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -2,53 +2,58 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const TodoModel = require('./models/Todo');
+require('dotenv').config(); // Load .env file
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
-    console.log('MongoDB connected')
-)
+// Use environment variable for MongoDB URI
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-app.listen(5000,
-    console.log('Server listening on port: 5000')
-)
+app.listen(5000, () => {
+    console.log('Server listening on port: 5000');
+});
 
 app.post('/add', (req, res) => {
   const { task } = req.body;
   TodoModel.create({ task })
-      .then(result => res.json(result))
-      .catch(err => console.log(err));
-   
+    .then(result => res.json(result))
+    .catch(err => console.log(err));
 });
 
-app.get('/get',(req,res)=>{
+app.get('/get', (req, res) => {
   TodoModel.find()
-  .then(result=> res.json(result))
-  .catch(err=>console.log(err));
+    .then(result => res.json(result))
+    .catch(err => console.log(err));
 });
-  
-app.put('/edit/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndUpdate(id,{done:true},{new:true})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
 
-app.put('/update/:id',(req,res)=>{
-  const{id} = req.params;
-  const{task} = req.body;
-  TodoModel.findByIdAndUpdate(id,{task:task})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
+app.put('/edit/:id', (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndUpdate(id, { done: true }, { new: true })
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
 
-app.delete('/delete/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndDelete({_id:id})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- }); 
+app.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+  TodoModel.findByIdAndUpdate(id, { task: task })
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
 
-module.exports=app;
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+module.exports = app;
+
